fd77191129ec0576264cb619a7a93153a30d0237
chao'L <475911599@qq.com>
2017-02-21 10:16:06 +0800

    Initial Commit

---
+++ judiciary/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  judiciary
+//
+//  Created by lecochao on 2017/2/21.
+//  Copyright © 2017年 yunduan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ judiciary/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  judiciary
+//
+//  Created by lecochao on 2017/2/21.
+//  Copyright © 2017年 yunduan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
